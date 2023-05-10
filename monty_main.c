74cb76df7d7957c39216403741bac7819764a39e
Flagelo6482 <frankln6482@gmail.com>
2023-05-09 17:21:35 -0700

    feat: Adding files :rocket:

---
+++ monty_main.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * main - Entry point
+ *
+ *@argc: Number of Arguments
+ *@argv: Argument list
+ *
+ *Return: (EXIT_SUCCESS) or (EXIT_FAILURE)
+ */
+
+int main(int argc, char **argv)
+{
+	
+}
