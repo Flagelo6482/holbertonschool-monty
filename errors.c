d96b8c2026dec3da9d66d0b2a19b30310479816f
Flagelo6482 <frankln6482@gmail.com>
2023-05-11 15:27:36 -0700

    feat: Error message archive :rocket:

---
+++ errors.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+int error_argc(void);
+
+
+/**
+ * error_argc - Print an error message
+ *
+ *Return: EXIT_FAILURE
+ */
+
+int error_argc(void)
+{
+	fprintf(stderr, "USAGE: monty file\n");
+	return (EXIT_FAILURE);
+}
+
+/**
+ * open_error - Open error message
+ *
+ *Return: EXIT_FAILURE
+ */
+
+int open_error(char *file_name)
+{
+	fprintf(stderr, "Error: Can't open file %s\n", file_name);
+	return (EXIT_FAILURE);
+}
